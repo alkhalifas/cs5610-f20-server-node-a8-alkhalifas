2233ef975ff68b1f82e4c5fac4ad30a4bc9b4d52
Saleh Alkhalifa <salehesam@gmail.com>
2020-11-24 15:28:03 -0500

    Configured for Heroku

--- controllers/questions-controller.js
+++ controllers/questions-controller.js
@@ -4,8 +4,8 @@ module.exports = (app) => {
 
     const findQuestionsForQuiz = (req, res) => {
         res.send(questionsService.findQuestionsForQuiz(req.params["qid"]))
-    }
+    };
 
     app.get("/quizzes/:qid/questions", findQuestionsForQuiz)
 
-}
+};
--- server.js
+++ server.js
@@ -5,7 +5,22 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers',
+               'Content-Type, X-Requested-With, Origin');
+    res.header('Access-Control-Allow-Methods',
+               'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+
+
 require("./controllers/questions-controller")(app)
 require("./controllers/quizzes-controller")(app)
 
-app.listen(3001)
+// app.listen(3001)
+
+index.listen(process.env.PORT || 3000, () =>
+    console.log("Server is running...")
+);
