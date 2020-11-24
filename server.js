8f7dc1e065ae18541f642fc931b80b384fccb6fd
Saleh Alkhalifa <salehesam@gmail.com>
2020-11-24 15:33:45 -0500

    Configured for Heroku, changed cors and port

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require("express")
 const app = express()
+var cors = require('cors');
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-               'Content-Type, X-Requested-With, Origin');
-    res.header('Access-Control-Allow-Methods',
-               'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
-});
+app.use(
+    cors({
+             credentials: true,
+             origin: true
+         })
+);
+app.options('*', cors());
 
 
 
@@ -21,6 +21,6 @@ require("./controllers/quizzes-controller")(app)
 
 // app.listen(3001)
 
-index.listen(process.env.PORT || 3000, () =>
+app.listen(process.env.PORT || 3000, () =>
     console.log("Server is running...")
 );
